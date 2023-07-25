1aac0b26f68cee5c31e450fa9de9d75a6a8e54e5
Pau Ibáñez <pauibanez2001@gmail.com>
2023-07-25 19:26:04 +0200

    updated the Errorr component's props

--- src/components/Errorr.tsx
+++ src/components/Errorr.tsx
@@ -1,12 +1,13 @@
 import { ErrorrOptions, StyleData } from "../types/types";
 
 interface Props {
-  content: string | JSX.Element | JSX.Element[];
+  content?: string;
+  children?: JSX.Element | JSX.Element[];
   styleData?: StyleData;
   options?: ErrorrOptions;
   name: string;
 }
 
-const Errorr = ({ content, name, styleData, options }: Props) => {};
+const Errorr = ({ content, name, styleData, options, children }: Props) => {};
 
 export default Errorr;
