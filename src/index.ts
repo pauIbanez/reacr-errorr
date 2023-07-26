f6bd5e6ceaab4efe4a19d0df204c17740dd58671
Pau Ibáñez <pauibanez2001@gmail.com>
2023-07-26 08:52:34 +0200

    added index.ts for importing

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+import Errorr from "./components/Errorr";
+import ErrorrContext from "./contexts/ErrorrContext.contectCreatior";
+import ErrorrContextProvider from "./contexts/ErrorrContextProvider";
+
+export { Errorr, ErrorrContext, ErrorrContextProvider };
