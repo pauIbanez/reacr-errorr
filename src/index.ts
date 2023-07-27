1f02f6d1cd4b045bba59e2b94fadca0d03af8056
Pau Ibáñez <pauibanez2001@gmail.com>
2023-07-27 16:01:00 +0200

    added export of useEffectOnce

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import Errorr from "./components/Errorr";
 import ErrorrContext from "./contexts/ErrorrContext.contectCreatior";
 import ErrorrContextProvider from "./contexts/ErrorrContextProvider";
+import useEffectOnce from "./hooks/useEffectOnce";
 
-export { Errorr, ErrorrContext, ErrorrContextProvider };
+export { Errorr, ErrorrContext, ErrorrContextProvider, useEffectOnce };
