7f9477fc66c68e712166ff4c503869485f4a42e1
Pau Ibáñez <pauibanez2001@gmail.com>
2023-09-06 18:46:59 +0200

    fixed recursive rendering on activateError

--- src/contexts/ErrorrContextProvider.tsx
+++ src/contexts/ErrorrContextProvider.tsx
@@ -1,4 +1,4 @@
-import { useCallback, useState } from "react";
+import { useCallback, useRef } from "react";
 import {
   ErrorrOptions,
   ErrorrContextData,
@@ -16,7 +16,7 @@ interface Props {
 }
 
 const ErrorrContextProvider = ({ children, options }: Props) => {
-  const [errorrs, setErrorrs] = useState<ErrorrData[]>([]);
+  const errorrs = useRef<ErrorrData[]>([]);
 
   const getOptions = (errorrOptions: ErrorrCreationOptions): ErrorrOptions => {
     const defaultOptions: ErrorrOptions = JSON.parse(
@@ -30,21 +30,22 @@ const ErrorrContextProvider = ({ children, options }: Props) => {
 
   const loadErrorr = (errorr: ErrorrCreationData) => {
     errorr.options = getOptions(errorr.options || {});
-    setErrorrs((prevErrorrs) => [...prevErrorrs, errorr as ErrorrData]);
+    errorrs.current = [...errorrs.current, errorr as ErrorrData];
   };
 
   const updateErrorr = (errorr: ErrorrCreationData) => {
     errorr.options = getOptions(errorr.options || {});
-    setErrorrs((prevErrorrs) =>
-      prevErrorrs.map((stateErrorr) =>
-        stateErrorr.name === errorr.name ? (errorr as ErrorrData) : stateErrorr
-      )
+
+    errorrs.current = errorrs.current.map((stateErrorr) =>
+      stateErrorr.name === errorr.name ? (errorr as ErrorrData) : stateErrorr
     );
   };
 
   const forceRemoveErrorr = useCallback(
     (name: string) => {
-      const foundErrorr = errorrs.find((errorr) => errorr.name === name);
+      const foundErrorr = errorrs.current.find(
+        (errorr) => errorr.name === name
+      );
       if (!foundErrorr) {
         if (options?.debug) console.error(`Errorr not found for name: ${name}`);
         return;
@@ -52,21 +53,21 @@ const ErrorrContextProvider = ({ children, options }: Props) => {
 
       foundErrorr.forceRemove();
 
-      setErrorrs((stateErrorrs) =>
-        stateErrorrs.map((e) => {
-          if (e.name === name) {
-            e.isActive = false;
-          }
-          return e;
-        })
-      );
+      errorrs.current = errorrs.current.map((errorr) => {
+        if (errorr.name === name) {
+          errorr.isActive = false;
+        }
+        return errorr;
+      });
     },
-    [errorrs, options?.debug]
+    [options?.debug]
   );
 
   const activateErrorr = useCallback(
     (name: string) => {
-      const foundErrorr = errorrs.find((errorr) => errorr.name === name);
+      const foundErrorr = errorrs.current.find(
+        (errorr) => errorr.name === name
+      );
       if (!foundErrorr) {
         if (options?.debug) console.error(`Errorr not found for name: ${name}`);
         return;
@@ -74,31 +75,27 @@ const ErrorrContextProvider = ({ children, options }: Props) => {
 
       foundErrorr.activate();
 
-      setErrorrs((stateErrorrs) =>
-        stateErrorrs.map((e) => {
-          if (e.name === name) {
-            e.isActive = true;
-          }
-          return e;
-        })
-      );
+      errorrs.current = errorrs.current.map((errorr) => {
+        if (errorr.name === name) {
+          errorr.isActive = true;
+        }
+        return errorr;
+      });
 
       setTimeout(() => {
-        setErrorrs((stateErrorrs) =>
-          stateErrorrs.map((e) => {
-            if (e.name === name) {
-              e.isActive = false;
-            }
-            return e;
-          })
-        );
+        errorrs.current = errorrs.current.map((errorr) => {
+          if (errorr.name === name) {
+            errorr.isActive = false;
+          }
+          return errorr;
+        });
       }, foundErrorr.options.activeTime);
     },
-    [errorrs, options?.debug]
+    [options?.debug]
   );
 
   const contextValue: ErrorrContextData = {
-    errorrs,
+    errorrs: errorrs.current,
     loadErrorr,
     updateErrorr,
     activateErrorr,
